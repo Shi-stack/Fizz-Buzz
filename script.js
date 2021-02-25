a79b078bd1b78f70d3210838fe50c0cd36f5c182
Shivani Patel <shivro0409@gmail.com>
2021-02-25 13:34:39 -0700

    JS file

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+for (i=1 ; i <= 100 ; i++){
+  
+  if (i % 3 === 0){
+    console.log("fizz");}
+  
+  else if (i % 5 === 0){
+    console.log ("buzz");}
+  
+  else if ( i % 15 === 0){
+    console.log ("fizzbuzz");}
+  
+  else    
+  {console.log(i);}
+  
+} 
+
+
+/*for (i = 1; i <= 100; i++) {
+  console.log(i);
+}*/
+
